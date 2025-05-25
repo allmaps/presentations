dc5f0e78fd7d3f358ca5541d00f34b5271719223
Bert Spaan <bert.spaan@gmail.com>
2025-05-25 12:58:18 +0200

    added empty iiif conference 2025 presentation

---
+++ presentations/2025/iiif-annual-conference/+page.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+  export const title = 'IIIF Annual Conference 2025 - Teaching with Allmaps'
+  export const date = Date.parse('4 June 2025')
+</script>
+
+<script lang="ts">
+  import Slide from '$lib/components/Slide.svelte'
+  import Title from '$lib/components/Title.svelte'
+  import MapMonster from '$lib/components/MapMonster.svelte'
+  import ManyMapMonsters from '$lib/components/ManyMapMonsters.svelte'
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
+
+<Title>
+  <h3 class="text-white">Teaching with Allmaps</h3>
+</Title>
