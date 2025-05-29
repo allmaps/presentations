38d1bd664c3fedd73cf7ebbb9411a11907a7e52e
Bert Spaan <bert.spaan@gmail.com>
2025-05-29 10:47:07 +0200

    added berts outline

--- presentations/2025/iiif-annual-conference/+page.svelte
+++ presentations/2025/iiif-annual-conference/+page.svelte
@@ -17,3 +17,29 @@
 <Title>
   <h3 class="text-white">Teaching with Allmaps</h3>
 </Title>
+
+<!-- Jules -->
+
+<!-- Bert -->
+<!--
+  - Iedereen wil een kaart vinden van z'n eigen huis!
+    - Show maps of a few of my houses, with marker?
+    - How to find these maps? Link pixels on digitized maps with geospatial coordinates
+  - Allmaps: georeferencing allows us to convert pixel coordinates to geospatial coordinates
+    - GCPs
+    - Interpolation, transformation algorithmms, with animation
+    - Laat kaart zien met driehoek, drie GCPs, en dan fade van ene algoritme naar andere
+  - Now we can warp maps and render them
+    - WebGL, Canvas API, XYZ tiles, print
+  - Allmaps Cursors
+  - En voorbeeld LMEC, oude straten op kaart
+  - Geospatial search!
+  - Allmaps Here
+    - Allmaps Here
+    - Find maps based on your current location
+    - Convert GPS location to pixel coordinates
+    - Render preview image with canvas renderer on Cloudflare Workers
+    - Met screenshot Signal, Slack, WhatsApp, etc.
+    - Try it now!
+  - En als ik nog tijd heb: projections?
+ -->
