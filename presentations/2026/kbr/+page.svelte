23777953bf7f052ac24be459be836773995085f0
Jules Schoonman <jules.schoonman@gmail.com>
2026-05-26 20:23:47 +0200

    change transformation type before map becomes visible

--- presentations/2026/kbr/+page.svelte
+++ presentations/2026/kbr/+page.svelte
@@ -437,6 +437,13 @@
                 '+proj=lcc +lat_1=35 +lat_2=65 +lat_0=52 +lon_0=10 +x_0=4000000 +y_0=2800000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs'
             }
           }
+        },
+        {
+          url: 'https://annotations.allmaps.org/maps/135dfd2d58dc26ec',
+          options: {
+            transformationType: 'polynomial',
+            opacity: 0
+          }
         }
       ]
     },
