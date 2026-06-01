d23218e32b385a78615d2159784f3135f24abbd9
Jules Schoonman <jules.schoonman@gmail.com>
2026-06-01 12:18:42 +0200

    updated presentation

--- presentations/2026/iiif-annual-conference/showcase/+page.svelte
+++ presentations/2026/iiif-annual-conference/showcase/+page.svelte
@@ -5,6 +5,7 @@
 </script>
 
 <script lang="ts">
+  import { pink, green } from '@allmaps/tailwind'
   import Slide from '$lib/components/Slide.svelte'
   import Title from '$lib/components/Title.svelte'
   import MapMonster from '$lib/components/MapMonster.svelte'
@@ -27,6 +28,32 @@
   <p class="text-white text-3xl font-light">IIIF Showcase</p>
 </Title>
 
+{#snippet face(src: string, color: string, name: string)}
+  <img
+    width="24"
+    class="relative inline-block rounded-full -top-[2%] size-[1.5em] border-4"
+    style:border-color={color}
+    {src}
+    alt={name}
+  />
+{/snippet}
+
+{#snippet bert()}
+  {@render face(
+    '/images/iiif-community-call-2025/bert.jpg',
+    pink,
+    'Bert Spaan'
+  )}
+{/snippet}
+
+{#snippet jules()}
+  {@render face(
+    '/images/iiif-community-call-2025/jules.jpg',
+    green,
+    'Jules Schoonman'
+  )}
+{/snippet}
+
 <!--
 Eerdere Aantekeningen Bert:
 Nog zoeken:
@@ -41,22 +68,31 @@ https://viewer.allmaps.org/?url=https://annotations.allmaps.org/images/f74a9c322
 -->
 
 <Slide preload>
-  <h2 class="font-medium">Contents</h2>
-  <ol>
-    <li>What is Allmaps?</li>
-    <li>How does Allmaps work?</li>
-    <li>What can you do with Allmaps?</li>
-    <li>How is the project organized?</li>
-  </ol>
+  <h2 class="font-light text-2xl">Contents</h2>
+  <div class="grid grid-rows-4 gap-4 text-xl font-bold">
+    <div class="bg-pink/5 p-4 md:p-8 rounded-xl">Why Allmaps?</div>
+    <div class="bg-blue/10 p-4 md:p-8 rounded-xl">How does Allmaps work?</div>
+    <div class="bg-green/10 p-4 md:p-8 rounded-xl">
+      What can you do with Allmaps?
+    </div>
+    <div class="bg-yellow/10 p-4 md:p-8 rounded-xl">
+      How is the project organized?
+    </div>
+  </div>
 </Slide>
 
 <Slide preload>
   <p>
-    Allmaps is an open source platform for <br />
+    Allmaps is an open source ecosystem for <br />
     curating, georeferencing and exploring IIIF maps
   </p>
+  <p class="fragment font-light text-2xl">
+    The project was founded in 2021 by independent cartographer {@render bert()}
+    Bert Spaan<br />and digital curator {@render jules()}
+    Jules Schoonman, TU Delft Library
+  </p>
 
-  <div class="fragment absolute top-0 left-0 w-full h-full text-left text-4xl">
+  <div class="fragment absolute top-0 left-0 w-full h-full text-left text-3xl">
     <div class="flex items-end h-full px-24 py-12">
       <MapMonster mood="happy" color="pink">
         <p class="p-4 max-w-xl">
@@ -76,14 +112,15 @@ https://viewer.allmaps.org/?url=https://annotations.allmaps.org/images/f74a9c322
 </Slide>
 
 <Slide preload>
-  <h2 class="font-medium">The goal of Allmaps</h2>
+  <h2 class="font-medium">Allmaps goals</h2>
 
-  <div class="max-w-6xl">
+  <div class="max-w-4xl">
     <span class="fragment text-pink">
       make <em>all</em> digitized maps more easy to find and use
     </span>
     <span class="fragment"
-      >● <span class="text-orange">make these maps searchable by location</span
+      >● <span class="text-orange"
+        >make these maps searchable by location in accessible web interfaces</span
       ></span
     >
     <span class="fragment"
@@ -92,8 +129,7 @@ https://viewer.allmaps.org/?url=https://annotations.allmaps.org/images/f74a9c322
       ></span
     >
     <span class="fragment"
-      >● <span>and finally, combine all of this in a single user interface</span
-      ></span
+      >● <span class="text-blue">close the gap between GIS and GLAM</span></span
     >
   </div>
 </Slide>
@@ -103,7 +139,7 @@ https://viewer.allmaps.org/?url=https://annotations.allmaps.org/images/f74a9c322
   chapters={[
     ...createExplainer(
       'https://annotations.allmaps.org/maps/e9aa6ec10276bf65@d2d0044d129ea2d5',
-      'Van Berckenrodekaart (17th century)',
+      'Van Berckenrodekaart (Allard Pierson)',
       { center: [4.897004, 52.3691733], zoom: 16, duration: 5000 }
     )
   ]}
@@ -294,52 +330,177 @@ https://viewer.allmaps.org/?url=https://annotations.allmaps.org/images/f74a9c322
     ...getExploreLayers('none')
   ]}
   chapters={[
+    // {
+    //   warpedMaps: {
+    //     url: 'https://annotations.allmaps.org/maps/b3872b1138d6830f',
+    //     caption: 'Galgenveld (Stadsarchief)',
+    //     useBearing: true,
+    //     options: {
+    //       // transformationType: 'helmert'
+    //     }
+    //   }
+    // },
+    // {
+    //   hideBasemap: false,
+    //   warpedMaps: [
+    //     {
+    //       caption: 'Het Watergraafs of Diemermeer',
+    //       useBearing: true,
+    //       url: 'https://annotations.allmaps.org/maps/8e571cf62c255d54@d526e9b963f79ef0'
+    //     }
+    //   ]
+    // },
     {
       hideBasemap: true,
-      warpedMaps: [
-        {
-          caption: 'Kermis op de Nieuwmarkt',
-          useBearing: true,
-          url: 'https://annotations.allmaps.org/images/bf6b564efcca3978',
-          options: {
-            applyMask: false,
-            removeColor: false,
-            transformationType: 'helmert'
-          }
+      warpedMaps: {
+        url: 'https://annotations.allmaps.org/images/7936d9f015e49c54',
+        caption: 'Loman Atlas (City Archive)',
+        useBearing: true,
+        options: {
+          applyMask: false,
+          renderMask: false,
+          renderGcps: false,
+          transformationType: 'helmert'
         }
-      ]
+      }
     },
     {
-      hideBasemap: false,
-      // layers: {
-      //   layer: 'allmaps_partners',
-      //   opacity: 1
-      // },
+      padding: 200,
+      warpedMaps: {
+        url: 'https://annotations.allmaps.org/images/7936d9f015e49c54',
+        caption: 'Loman Atlas (City Archive)'
+      }
+    },
+    {
+      caption: 'Works for multiple map sheets!',
+      warpedMaps: {
+        url: 'https://annotations.allmaps.org/manifests/af012d4792961902',
+        caption: 'Loman Atlas (City Archive)',
+        options: {
+          transformationType: 'polynomial',
+          renderMask: false,
+          removeColor: false,
+          renderGcps: false
+        }
+      }
+    },
+    {
+      caption: 'Works for multiple map sheets!',
+      warpedMaps: {
+        url: 'https://annotations.allmaps.org/manifests/af012d4792961902',
+        caption: 'Loman Atlas (City Archive)',
+        options: {
+          transformationType: 'polynomial',
+          removeColor: false,
+          renderMask: true,
+          renderMaskSize: 4,
+          saturation: 1
+        }
+      }
+    },
+    {
+      location: {
+        zoom: 17,
+        center: [4.899895, 52.359702]
+      },
+      warpedMaps: {
+        url: 'https://annotations.allmaps.org/manifests/af012d4792961902',
+        caption: 'Loman Atlas (City Archive)',
+        options: {
+          transformationType: 'polynomial',
+          renderMask: false,
+          removeColor: true,
+          removeColorColor: '#EEDECA',
+          removeColorThreshold: 0.2,
+          saturation: 0
+        }
+      }
+    },
+    {
       warpedMaps: [
         {
-          caption: 'Kermis op de Nieuwmarkt',
-          useBearing: true,
-          url: 'https://annotations.allmaps.org/images/bf6b564efcca3978',
+          url: 'https://annotations.allmaps.org/maps/4e838295a6f98db5',
+          caption: 'Plattegrond van het Paleis voor Volksvlijt (Rijksmuseum)',
+          useBounds: true,
           options: {
-            applyMask: true,
+            renderGcps: true
+          }
+        },
+        {
+          url: 'https://annotations.allmaps.org/manifests/af012d4792961902',
+          caption: 'Loman Atlas (City Archive)',
+          options: {
             transformationType: 'polynomial',
+            renderMask: false,
             removeColor: true,
-            removeColorColor: '#91908A',
-            removeColorThreshold: 0.2
+            removeColorColor: '#EEDECA',
+            removeColorThreshold: 0.2,
+            saturation: 0
           }
         }
       ]
     },
     {
+      warpedMaps: [
+        {
+          url: 'https://annotations.allmaps.org/images/91418b2b2ddc304f ',
+          caption:
+            'Plattegrond van de Internationale Tentoonstelling van Tuinbouw (Rijksmuseum)'
+        },
+        {
+          url: 'https://annotations.allmaps.org/maps/4e838295a6f98db5',
+          caption: 'Plattegrond van het Paleis voor Volksvlijt (Rijksmuseum)',
+          useBounds: true,
+          options: {
+            renderGcps: false
+          }
+        },
+        {
+          url: 'https://annotations.allmaps.org/manifests/af012d4792961902',
+          caption: 'Loman Atlas (City Archive)',
+          options: {
+            transformationType: 'polynomial',
+            renderMask: false,
+            removeColor: true,
+            removeColorColor: '#EEDECA',
+            removeColorThreshold: 0.2,
+            saturation: 0
+          }
+        }
+      ]
+    },
+    {
+      warpedMaps: [
+        {
+          url: 'https://annotations.allmaps.org/maps/fedf952c51cb5d1a',
+          caption: 'Plattegrond van het Paleis voor Volksvlijt (Rijksmuseum)',
+          useBounds: true
+        }
+      ]
+    },
+    {
+      hideBasemap: false,
+      warpedMaps: {
+        caption:
+          'Officieel Plan der Internationale Koloniale Tentoonstelling (City Archive)',
+        useBearing: true,
+        url: 'https://annotations.allmaps.org/maps/410c02766b8b14d5@cb41d7ce5c1cefe7',
+        options: {
+          applyMask: true,
+          transformationType: 'polynomial',
+          removeColor: false
+        }
+      }
+    },
+    {
       hideBasemap: false,
       warpedMaps: [
         {
-          caption: 'Officieel Plan der Tentoonstelling',
+          caption:
+            'Lijnen der Amsterdamsche Tram Omnibus-Maatschappij (City Archive)',
           useBearing: true,
-          url: 'https://annotations.allmaps.org/maps/410c02766b8b14d5@cb41d7ce5c1cefe7',
+          url: 'https://annotations.allmaps.org/maps/d45be887a1641291@333de2b0663c4257',
           options: {
-            applyMask: true,
-            transformationType: 'polynomial',
             removeColor: false
           }
         }
@@ -349,9 +510,14 @@ https://viewer.allmaps.org/?url=https://annotations.allmaps.org/images/f74a9c322
       hideBasemap: false,
       warpedMaps: [
         {
-          caption: 'Het Watergraafs of Diemermeer',
+          caption:
+            'Ontwerp eener Verbindingslijn van de Spoorwegen te Amsterdam (City Archive)',
           useBearing: true,
-          url: 'https://annotations.allmaps.org/maps/8e571cf62c255d54@d526e9b963f79ef0'
+          url: 'https://annotations.allmaps.org/maps/ef8a0d3ccd0d32ab@5f5ac163231178ff',
+          options: {
+            applyMask: true,
+            removeColor: false
+          }
         }
       ]
     },
@@ -359,7 +525,72 @@ https://viewer.allmaps.org/?url=https://annotations.allmaps.org/images/f74a9c322
       hideBasemap: false,
       warpedMaps: [
         {
-          caption: 'Stadsdeel Zuidoost',
+          caption:
+            'Plan Van Niftrik tot uitbreiding van Amsterdam (City Archive)',
+          useBearing: true,
+          url: 'https://annotations.allmaps.org/maps/d3d62a3963e87919@bf264681b87dfeae',
+          options: {
+            applyMask: true,
+            removeColor: false
+          }
+        }
+      ]
+    },
+    {
+      hideBasemap: false,
+      warpedMaps: [
+        {
+          caption:
+            'Plan Van Niftrik tot uitbreiding van Amsterdam (City Archive)',
+          useBearing: true,
+          url: 'https://annotations.allmaps.org/maps/d3d62a3963e87919@bf264681b87dfeae',
+          options: {
+            applyMask: true,
+            removeColor: true,
+            removeColorColor: '#ECDFC4',
+            removeColorThreshold: 0.2
+          }
+        }
+      ]
+    },
+    {
+      caption: 'Not just for old maps!',
+      warpedMaps: {
+        url: 'https://cityatlas.theberlage.nl/data/03-argumentation/01-uilenburg/annotations/200uilenburg.json',
+        caption: 'The Berlage City Atlas (TU Delft)'
+      }
+    },
+    {
+      warpedMaps: {
+        caption: 'Kermis op de Nieuwmarkt (City Archive)',
+        useBearing: true,
+        url: 'https://annotations.allmaps.org/images/bf6b564efcca3978',
+        options: {
+          applyMask: false,
+          removeColor: false,
+          transformationType: 'helmert'
+        }
+      }
+    },
+    {
+      warpedMaps: {
+        caption: 'Kermis op de Nieuwmarkt (City Archive)',
+        useBearing: true,
+        url: 'https://annotations.allmaps.org/images/bf6b564efcca3978',
+        options: {
+          applyMask: true,
+          transformationType: 'polynomial',
+          removeColor: true,
+          removeColorColor: '#91908A',
+          removeColorThreshold: 0.2
+        }
+      }
+    },
+    {
+      hideBasemap: false,
+      warpedMaps: [
+        {
+          caption: 'Stadsdeel Zuidoost (City Archive)',
           useBearing: true,
           url: 'https://annotations.allmaps.org/maps/49cef0f152f5e26b@3c8174894e4382e5',
           options: {
@@ -376,7 +607,7 @@ https://viewer.allmaps.org/?url=https://annotations.allmaps.org/images/f74a9c322
       },
       warpedMaps: [
         {
-          caption: 'Stadsdeel Zuidoost',
+          caption: 'Stadsdeel Zuidoost (City Archive)',
           // useBearing: true,
           url: 'https://annotations.allmaps.org/maps/49cef0f152f5e26b@3c8174894e4382e5',
           options: {
@@ -391,65 +622,8 @@ https://viewer.allmaps.org/?url=https://annotations.allmaps.org/images/f74a9c322
       hideBasemap: false,
       warpedMaps: [
         {
-          caption: 'Lijnen der Amsterdamsche Tram Omnibus-Maatschappij',
-          useBearing: true,
-          url: 'https://annotations.allmaps.org/maps/d45be887a1641291@333de2b0663c4257',
-          options: {
-            removeColor: false
-          }
-        }
-      ]
-    },
-    {
-      hideBasemap: false,
-      warpedMaps: [
-        {
-          caption: 'Plan tot uitbreiding van Amsterdam (Van Niftrik)',
-          useBearing: true,
-          url: 'https://annotations.allmaps.org/maps/d3d62a3963e87919@bf264681b87dfeae',
-          options: {
-            applyMask: true,
-            removeColor: false
-          }
-        }
-      ]
-    },
-    {
-      hideBasemap: false,
-      warpedMaps: [
-        {
-          caption: 'Plan tot uitbreiding van Amsterdam (Van Niftrik)',
-          useBearing: true,
-          url: 'https://annotations.allmaps.org/maps/d3d62a3963e87919@bf264681b87dfeae',
-          options: {
-            applyMask: true,
-            removeColor: true,
-            removeColorColor: '#ECDFC4',
-            removeColorThreshold: 0.2
-          }
-        }
-      ]
-    },
-    {
-      hideBasemap: false,
-      warpedMaps: [
-        {
           caption:
-            'Ontwerp eener Verbindingslijn van de Spoorwegen te Amsterdam',
-          useBearing: true,
-          url: 'https://annotations.allmaps.org/maps/ef8a0d3ccd0d32ab@5f5ac163231178ff',
-          options: {
-            applyMask: true,
-            removeColor: false
-          }
-        }
-      ]
-    },
-    {
-      hideBasemap: false,
-      warpedMaps: [
-        {
-          caption: 'Uitbreidingsplan van de Gemeente Watergraafsmeer',
+            'Uitbreidingsplan van de Gemeente Watergraafsmeer (City Archive)',
           useBearing: true,
           url: 'https://annotations.allmaps.org/maps/8403065303f57c32@26536be2d169d544',
           options: {
@@ -463,7 +637,7 @@ https://viewer.allmaps.org/?url=https://annotations.allmaps.org/images/f74a9c322
       hideBasemap: false,
       warpedMaps: [
         {
-          caption: 'Plan Havenwerken te Amsterdam',
+          caption: 'Plan Havenwerken te Amsterdam (City Archive)',
           useBearing: true,
           url: 'https://annotations.allmaps.org/maps/ebd85babaaabc34d@70b4bd5b7b5117cb',
           options: {
@@ -477,7 +651,7 @@ https://viewer.allmaps.org/?url=https://annotations.allmaps.org/images/f74a9c322
       hideBasemap: false,
       warpedMaps: [
         {
-          caption: 'Uitbreiding van Amsterdam',
+          caption: 'Uitbreiding van Amsterdam (City Archive)',
           useBearing: true,
           url: 'https://annotations.allmaps.org/maps/1260a5102188c1b4@13b7dc87cce0b1af',
           options: {
@@ -492,7 +666,7 @@ https://viewer.allmaps.org/?url=https://annotations.allmaps.org/images/f74a9c322
       warpedMaps: [
         {
           caption:
-            'Uitlegging der Stad Amsterdam aan de IJ-zijde met een brug over het IJ',
+            'Uitlegging der Stad Amsterdam aan de IJ-zijde met een brug over het IJ (City Archive)',
           useBearing: true,
           url: 'https://annotations.allmaps.org/maps/4f04559e2fc4d4f1@648323ff2bf61c1d',
           options: {
@@ -506,8 +680,7 @@ https://viewer.allmaps.org/?url=https://annotations.allmaps.org/images/f74a9c322
       hideBasemap: false,
       warpedMaps: [
         {
-          caption:
-            'Amsterdam Uitgelegen aan de IJ-zijde, Ontwerp van JSON. Galman',
+          caption: 'Amsterdam Uitgelegen aan de IJ-zijde (City Archive)',
           useBearing: true,
           url: 'https://annotations.allmaps.org/maps/fc1b4c981beba120@8b5697db7b406081',
           options: {
@@ -521,7 +694,7 @@ https://viewer.allmaps.org/?url=https://annotations.allmaps.org/images/f74a9c322
       hideBasemap: false,
       warpedMaps: [
         {
-          caption: 'Ontwerp van een brug over het IJ',
+          caption: 'Ontwerp van een brug over het IJ (City Archive)',
           useBearing: true,
           url: 'https://annotations.allmaps.org/maps/71628683c7e65a64@4784e6a3f5467075',
           options: {
@@ -548,7 +721,7 @@ https://viewer.allmaps.org/?url=https://annotations.allmaps.org/images/f74a9c322
       },
       warpedMaps: [
         {
-          caption: 'Ontwerp van een brug over het IJ',
+          caption: 'Ontwerp van een brug over het IJ (City Archive)',
           useBearing: true,
           url: 'https://annotations.allmaps.org/maps/71628683c7e65a64@4784e6a3f5467075',
           options: {
@@ -594,6 +767,29 @@ https://viewer.allmaps.org/?url=https://annotations.allmaps.org/images/f74a9c322
   ></iframe>
 </Slide>
 
+<Slide preload>
+  <h2 class="font-medium">Partnership goals</h2>
+  <div class="grid grid-cols-4 gap-4 text-xl font-light">
+    <div class="bg-pink/5 p-4 md:p-8 rounded-xl">
+      Support maintenance & development of Allmaps as <span class="font-medium"
+        >open infrastructure</span
+      >
+    </div>
+    <div class="bg-blue/10 p-4 md:p-8 rounded-xl">
+      Introduce <span class="font-medium">institutional benefits</span> that ease
+      integrations without limiting overall openness
+    </div>
+    <div class="bg-green/10 p-4 md:p-8 rounded-xl">
+      Promote IIIF membership, Allmaps implementations, and create a shared
+      discovery layer for <span class="font-medium">IIIF Commons</span>
+    </div>
+    <div class="bg-yellow/10 p-4 md:p-8 rounded-xl">
+      Implement a <span class="font-medium">governance structure</span> through the
+      establishment of the Allmaps Advisory Board
+    </div>
+  </div>
+</Slide>
+
 <MapSlide
   sources={{
     allmaps_partners: {
@@ -618,7 +814,9 @@ https://viewer.allmaps.org/?url=https://annotations.allmaps.org/images/f74a9c322
         'text-ignore-placement': true
       },
       paint: {
-        'text-color': '#000000',
+        'text-color': '#2b2b2b',
+        'text-halo-color': '#ffffff',
+        'text-halo-width': 2,
         'text-opacity': [
           'interpolate',
           ['linear'],
