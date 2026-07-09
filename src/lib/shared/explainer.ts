db934e43aebfb286fe1b4db883656a644fb681bf
Jules Schoonman <jules.schoonman@gmail.com>
2026-07-09 10:07:42 +0200

    Add prop to disable tps slides

--- src/lib/shared/explainer.ts
+++ src/lib/shared/explainer.ts
@@ -1,13 +1,84 @@
 import type { MapChapterProps } from '$lib/types/warped-map'
+import { slide } from 'svelte/transition'
 
 const explainer = (options: {
   annotationUrl: string
   caption?: string
   homepage: string
   tpsLocation?: MapChapterProps['location']
+  includeTps?: boolean
 }) => {
-  const { annotationUrl, caption, homepage, tpsLocation } = options
-  return [
+  const {
+    annotationUrl,
+    caption,
+    homepage,
+    includeTps = true,
+    tpsLocation
+  } = options
+  const tpsSlides = [
+    {
+      location: tpsLocation,
+      caption: 'Analyzing map distortions',
+      padding: 25,
+      warpedMaps: [
+        {
+          url: annotationUrl,
+          caption,
+          homepage,
+          options: {
+            applyMask: true,
+            renderGcps: true,
+            renderTransformedGcps: true,
+            renderVectors: true,
+            renderVectorsColor: '#ffffff',
+            transformationType: 'polynomial',
+            opacity: 0.8
+          }
+        }
+      ]
+    },
+    {
+      location: tpsLocation,
+      caption: 'A better fit?',
+      padding: 25,
+      warpedMaps: [
+        {
+          url: annotationUrl,
+          caption,
+          homepage,
+          options: {
+            applyMask: true,
+            renderGcps: true,
+            renderTransformedGcps: true,
+            renderVectors: true,
+            transformationType: 'thinPlateSpline',
+            opacity: 0.8
+          }
+        }
+      ]
+    },
+    {
+      location: tpsLocation,
+      caption: 'A better fit?',
+      padding: 25,
+      warpedMaps: [
+        {
+          url: annotationUrl,
+          caption,
+          homepage,
+          options: {
+            applyMask: true,
+            renderGcps: true,
+            renderTransformedGcps: true,
+            renderVectors: true,
+            transformationType: 'thinPlateSpline',
+            opacity: 1
+          }
+        }
+      ]
+    }
+  ] satisfies MapChapterProps[]
+  const slides = [
     {
       caption: 'IIIF Image',
       hideBasemap: true,
@@ -74,69 +145,9 @@ const explainer = (options: {
           }
         }
       ]
-    },
-    {
-      location: tpsLocation,
-      caption: 'Analyzing map distortions',
-      padding: 25,
-      warpedMaps: [
-        {
-          url: annotationUrl,
-          caption,
-          homepage,
-          options: {
-            applyMask: true,
-            renderGcps: true,
-            renderTransformedGcps: true,
-            renderVectors: true,
-            renderVectorsColor: '#ffffff',
-            transformationType: 'polynomial',
-            opacity: 0.8
-          }
-        }
-      ]
-    },
-    {
-      location: tpsLocation,
-      caption: 'A better fit?',
-      padding: 25,
-      warpedMaps: [
-        {
-          url: annotationUrl,
-          caption,
-          homepage,
-          options: {
-            applyMask: true,
-            renderGcps: true,
-            renderTransformedGcps: true,
-            renderVectors: true,
-            transformationType: 'thinPlateSpline',
-            opacity: 0.8
-          }
-        }
-      ]
-    },
-    {
-      location: tpsLocation,
-      caption: 'A better fit?',
-      padding: 25,
-      warpedMaps: [
-        {
-          url: annotationUrl,
-          caption,
-          homepage,
-          options: {
-            applyMask: true,
-            renderGcps: true,
-            renderTransformedGcps: true,
-            renderVectors: true,
-            transformationType: 'thinPlateSpline',
-            opacity: 1
-          }
-        }
-      ]
     }
   ] satisfies MapChapterProps[]
+  return includeTps ? slides.concat(tpsSlides) : slides
 }
 
 export default explainer
